3a1732da5676bf4356e88ec8951fe559808775d4
jacksonvong <535433868@qq.com>
2019-12-03 13:52:19 +0800

    deploy

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# git init
+git pull git@github.com:jacksonvong/iways-ui.git master
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:jacksonvong/iways-ui.git master
+
+cd -
